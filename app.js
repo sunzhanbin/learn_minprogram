9b5fcfb020643df526c2ee7b45c2d58125b5b09f
sunbinbin <sunbb@neocrm.com>
2022-02-12 15:54:02 +0800

    fix: 初始化小程序项目

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ App({
     // 登录
     wx.login({
       success: res => {
+        debugger
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        console.log(res);
       }
     })
   },
